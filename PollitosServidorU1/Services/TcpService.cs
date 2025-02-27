61591537ce556fc9120ec0c925b53a2b623a5bc2
LuisRiveraGzz563 <luisriverapto@outlook.com>
2025-02-27 14:43:49 -0600

    Tablero mostrandose y configuracion inicial de Servidor TCP

---
+++ PollitosServidorU1/Services/TcpService.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using PollitosServidorU1.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace PollitosServidorU1.Services
+{
+    public class TcpService
+    {
+        //Creamos un objeto de tipo TcpListener
+        private readonly TcpListener listener;
+        private readonly List<TcpClient> Clientes = new List<TcpClient>();
+        public event Action<PollitoDTO> PollitoRecibido;
+        public TcpService()
+        {
+            //Inicializamos el objeto listener con la IP Any y el puerto 5000
+            listener = new TcpListener(IPAddress.Any, 5000);
+            //Iniciamos el listener
+            listener.Start();
+            //Recibimos clientes
+            RecibirClientes();
+        }
+
+        private void RecibirClientes()
+        {
+            TcpClient tcpClient = listener.AcceptTcpClient();
+            //Crear un hilo para escuchar a los clientes
+            Thread t = new Thread(Escuchar);
+        }
+
+        private void Escuchar(object tcpClient)
+        {
+            if (tcpClient != null)
+            {
+                var client = (TcpClient)tcpClient;
+                //Agregamos el cliente a la lista de clientes
+                Clientes.Add(client);
+                while (client.Connected)
+                {
+                    if (client.Available > 0)
+                    {
+                        //Creamos un buffer para recibir los datos
+                        byte[] buffer = new byte[client.Available];
+                        //Leemos los datos del cliente y guardar los datos en el buffer
+                        client.GetStream().Read(buffer, 0, buffer.Length);
+                        //Convertimos los datos a string
+                        var json = Encoding.UTF8.GetString(buffer);
+                        //Convertimos el json a un objeto
+                        var pollito = JsonConvert.DeserializeObject<PollitoDTO>(json);
+
+                        if (pollito != null)
+                        {
+                            //Invocamos el evento PollitoRecibido
+                            PollitoRecibido?.Invoke(pollito);
+                            try
+                            {
+                                //Enviamos la lista de clientes a todos los clientes
+                                foreach (var c in Clientes)
+                                {
+                                    c?.Client.Send(buffer);
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                //Si hay un error, eliminamos el cliente de la lista
+                                Clientes.Remove(client);
+                            }
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
--- PollitosServidorU1/ViewModels/Corral.cs
+++ PollitosServidorU1/ViewModels/Corral.cs
@@ -1,4 +1,6 @@
 ﻿using PollitosServidorU1.Models;
+using PollitosServidorU1.Services;
+using System;
 using System.Collections.Generic;
 
 namespace PollitosServidorU1.ViewModels
@@ -7,22 +9,49 @@ namespace PollitosServidorU1.ViewModels
     {
         public List<PollitoDTO> Pollos { get; set; } = new List<PollitoDTO>();
         public PollitoDTO Pollo { get; set; }
+        public TcpService Servidor { get; set; } = new TcpService();
+
+        private readonly int NumMaiz = 5;
+        private readonly Random r = new Random();
         public Corral()
         {
             for (int i = 0; i < 25; i++)
             {
                 Pollos.Add(null);
             }
-
-            Pollos[1]= new PollitoDTO
+            //Esto muestra un usuario de muestra
+            Pollos[1] = new PollitoDTO
             {
                 Imagen = "🐥",
                 Nombre = "Pollito 1",
-                Puntuacion = 100,
-                Posicion = 1,
+                Puntuacion = 0,
                 IP = "192.168.1.1"
             };
+            GenerarMaiz();
 
+            Servidor.PollitoRecibido += Servidor_PollitoRecibido;
+        }
+
+        private void Servidor_PollitoRecibido(PollitoDTO dto)
+        {
+            Pollos[dto.Posicion] = dto;
+        }
+
+        //Metodo para generar maiz de manera aleatoria en el corral
+        void GenerarMaiz()
+        {
+            for (int i = 0; i < NumMaiz; i++)
+            {
+                int posicion = r.Next(0, 25);
+                if (Pollos[posicion] == null)
+                {
+                    Pollos[posicion] = new PollitoDTO
+                    {
+                        Imagen = "🌽",
+                        Puntuacion = -10,
+                    };
+                }
+            }
         }
     }
 }
