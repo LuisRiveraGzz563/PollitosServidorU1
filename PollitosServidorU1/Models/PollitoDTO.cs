af059369a931f8983194d9c3e8189505e64ef2ac
LuisRiveraGzz563 <luisriverapto@outlook.com>
2025-02-27 12:21:09 -0600

    Agregar archivos de proyecto.

---
+++ PollitosServidorU1/Models/PollitoDTO.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PollitosServidorU1.Models
+{
+    public partial class PollitoDTO: ObservableObject
+    {
+        [ObservableProperty]
+        public string Imagen { get; set; }
+        [ObservableProperty]
+        public string Nombre { get; set; }
+        [ObservableProperty]
+        public int Puntuacion { get; set; }
+        [ObservableProperty]
+        public int Posicion { get; set; }
+        [ObservableProperty]
+        public string IP { get; set; }
+    }
+}
---
+++ PollitosServidorU1/ViewModels/Corral.cs
@@ -0,0 +1,28 @@
+﻿using PollitosServidorU1.Models;
+using System.Collections.Generic;
+
+namespace PollitosServidorU1.ViewModels
+{
+    public class Corral
+    {
+        public List<PollitoDTO> Pollos { get; set; } = new List<PollitoDTO>();
+        public PollitoDTO Pollo { get; set; }
+        public Corral()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Pollos.Add(null);
+            }
+
+            Pollos[1]= new PollitoDTO
+            {
+                Imagen = "🐥",
+                Nombre = "Pollito 1",
+                Puntuacion = 100,
+                Posicion = 1,
+                IP = "192.168.1.1"
+            };
+
+        }
+    }
+}
