51961eb58499f72dcd551b04eb9a3fce7d14906a
LuisRiveraGzz563 <luisriverapto@outlook.com>
2025-03-02 16:40:09 -0600

    Servidor en  teoria funcionando

--- PollitosServidorU1/ViewModels/Corral.cs
+++ PollitosServidorU1/ViewModels/Corral.cs
@@ -89,21 +89,27 @@ namespace PollitosServidorU1.ViewModels
             if (Pollo == null) return;
 
             int nuevaPosicion = Pollo.Posicion;
-
+            //Asignar la nueva posicion dependiendo de la direccion
             switch (direccion)
             {
+                // Arriba
                 case 1 when nuevaPosicion >= Columnas: nuevaPosicion -= Columnas; break;
+                // Abajo
                 case 2 when nuevaPosicion < (TamañoCorral - Columnas): nuevaPosicion += Columnas; break;
+                // Izquierda
                 case 3 when nuevaPosicion % Columnas != 0: nuevaPosicion -= 1; break;
+                // Derecha
                 case 4 when nuevaPosicion % Columnas != (Columnas - 1): nuevaPosicion += 1; break;
             }
 
+            // Verificar si hay maíz en la nueva posición
             if (Pollos[nuevaPosicion] != null && Pollos[nuevaPosicion].Puntuacion == -10)
             {
                 Pollo.Puntuacion++;
+                // Eliminar el maíz
                 Pollos[nuevaPosicion] = Pollo;
                 Pollo.Posicion = nuevaPosicion;
-
+                // Generar un nuevo maíz
                 GenerarNuevoMaiz();
             }
             else if (Pollos[nuevaPosicion] == null)
